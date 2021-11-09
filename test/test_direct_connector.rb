3f7a4df6da6617eec3daafff9d82febb2e7095ad
Ichinari Sato <ichinari-sato@cookpad.com>
2021-11-09 18:08:34 +0900

    Add type cast option test for transport_delta and transport_all

--- test/test_direct_connector.rb
+++ test/test_direct_connector.rb
@@ -23,4 +23,33 @@ class TestConnector < Test::Unit::TestCase
     )
     job.execute
   end
+
+  def test_enable_transport_delta
+    data_date = '2016-11-03'
+    job = RedshiftConnector.transport_delta(
+      schema: $TEST_SCHEMA,
+      table: 'item_pvs',
+
+      txn_id: data_date,
+      condition: %Q(data_date = date '#{data_date}'),
+      delete_cond: %Q(data_date = date '#{data_date}'),
+
+      columns: %w[id data_date item_id pv uu],
+      enable_cast: true
+    )
+    job.execute
+  end
+
+  def test_enable_transport_all
+    data_date = '2016-11-03'
+    job = RedshiftConnector.transport_all(
+      strategy: 'truncate',
+      schema: $TEST_SCHEMA,
+      table: 'item_pvs',
+      txn_id: data_date,
+      columns: %w[id data_date item_id pv uu],
+      enable_cast: true
+    )
+    job.execute
+  end
 end
